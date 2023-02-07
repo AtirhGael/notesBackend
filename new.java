83d44cc656c9d5d49301602268a73e2cd17e515c
AtirhGael <atirhgael874@gmail.com>
2023-02-07 06:38:10 +0100

    second branch

--- new.java
+++ new.java
@@ -1,10 +1,5 @@
-public class IfExample {  
-public static void main(String[] args) {  
-    //defining an 'age' variable  
-    int age=20;  
-    //checking the age  
-    if(age>18){  
-        System.out.print("Age is greater than 18");  
-    }  
-}  
-}  
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
